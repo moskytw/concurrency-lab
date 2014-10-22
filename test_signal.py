aa8897188e3094628323becc99fb76e1432851aa
Mosky <mosky.tw@gmail.com>
2014-10-23 01:04:56 +0800

    better

--- test_signal.py
+++ test_signal.py
@@ -2,20 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from sys import exit
-from os import getpid, fork
+from os import getpid, fork, _exit
 from time import sleep
-from signal import signal, SIGINT, SIGKILL, SIGTERM
+from signal import signal, SIGINT, SIGKILL, SIGTERM, SIG_DFL
 
-def print_signal(signum, frame):
-    print "I'm {}. I just caught {}. I am leaving.".format(getpid(), signum)
-    exit()
+cpid = None
 
-signal(SIGINT, print_signal)
+def print_n_exit(signum, frame):
+    if cpid is None or cpid == 0:
+        print "I'm {}. I just caught {}. I am cleaning up ...".format(getpid(), signum)
+        exit(signum)
+    else:
+        print "I'm {}. I just caught {}. Bye.".format(getpid(), signum)
+        _exit(signum)
+
+signal(SIGINT, print_n_exit)
 # can't be caught
 #signal(SIGKILL, print_signal)
-signal(SIGTERM, print_signal)
+signal(SIGTERM, print_n_exit)
 
-if __name__ == '__main__':
+def main():
+
+    global cpid
 
     cpid = fork()
     print "I'm {}. I forked {}.".format(getpid(), cpid)
@@ -23,3 +31,7 @@ if __name__ == '__main__':
     while 1:
         print "I'm {}. I'm alive.".format(getpid())
         sleep(1)
+
+if __name__ == '__main__':
+    main()
+
