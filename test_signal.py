205abee4901f08bc802ee077faea34d6212b7634
Mosky <mosky.tw@gmail.com>
2014-10-23 01:41:02 +0800

    just reset as default

--- test_signal.py
+++ test_signal.py
@@ -2,19 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from sys import exit
-from os import getpid, fork, _exit
+from os import getpid, fork
 from time import sleep
 from signal import signal, SIGINT, SIGKILL, SIGTERM, SIG_DFL
 
-cpid = None
-
 def print_n_exit(signum, frame):
-    if cpid is None or cpid == 0:
-        print "I'm {}. I just caught {}. I am cleaning up ...".format(getpid(), signum)
-        exit(signum)
-    else:
-        print "I'm {}. I just caught {}. Bye.".format(getpid(), signum)
-        _exit(signum)
+    print "I'm {}. I just caught {}. I am leaving ...".format(getpid(), signum)
+    exit(signum)
 
 signal(SIGINT, print_n_exit)
 # can't be caught
@@ -23,9 +17,11 @@ signal(SIGTERM, print_n_exit)
 
 def main():
 
-    global cpid
-
     cpid = fork()
+    if cpid == 0:
+        signal(SIGINT, SIG_DFL)
+        signal(SIGTERM, SIG_DFL)
+
     print "I'm {}. I forked {}.".format(getpid(), cpid)
 
     while 1:
